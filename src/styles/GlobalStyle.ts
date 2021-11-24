105d53a88a679bcd043205558bddc10a14c1ff4e
Caio Tracera <ctrcra@gmail.com>
2021-11-24 11:51:08 -0300

    style: add accessibility to font-size

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -20,10 +20,14 @@ export const GlobalStyle = createGlobalStyle`
 
   html {
     @media (max-width: 1080px) {
-      font-size: 93.75%; // 15px
+      html {
+        font-size: 93.75%; // 15px
+      }
     }
     @media (max-width: 768px) {
-      font-size: 87.5%; // 14px
+      html {
+        font-size: 87.5%; // 14px
+      }
     }
   }
 
